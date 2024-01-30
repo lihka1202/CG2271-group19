27659a5cdfea691aa64315cdec71d6d3c390f772
lihka1202 <88475310+lihka1202@users.noreply.github.com>
2024-01-30 13:41:50 +0800

    feat(lab): Lab1 complete

--- CG2271-Lab2/myBlinky.c
+++ CG2271-Lab2/myBlinky.c
@@ -6,6 +6,15 @@
 #define BLUE_LED							1
 #define MASK(x)								(1 << (x))
 
+
+static void delay(volatile uint32_t nof) {
+	while(nof!=0) {
+		__asm("NOP");
+		nof--;
+	}
+}
+
+
 void InitGPIO(void) {
 	// Enable Clock to PORTB and PORTD
 	SIM->SCGC5 |= ((SIM_SCGC5_PORTB_MASK)| (SIM_SCGC5_PORTD_MASK));
@@ -27,6 +36,37 @@ void InitGPIO(void) {
 	PTD->PDDR |= MASK(BLUE_LED);
 	
 }
+void TurnLEDOnAndOff(LED) {
+	if(LED==RED_LED || LED==GREEN_LED) {
+		PTB->PDOR &= ~MASK(LED);
+	} else {
+		PTD->PDOR &= ~MASK(LED);
+	}
+	
+	delay(0xF0000);
+	
+	if(LED==RED_LED || LED==GREEN_LED) {
+		PTB->PDOR |= MASK(LED);
+	} else {
+		PTD->PDOR |= MASK(LED);
+	}
+	
+}
+
+void controlLed(){
+	int flag = 0;
+	switch(flag) {
+		case 0:
+			TurnLEDOnAndOff(RED_LED);
+			flag+=1;
+		case 1:
+			TurnLEDOnAndOff(BLUE_LED);
+			flag+=1;
+		case 2:
+			TurnLEDOnAndOff(GREEN_LED);
+			flag=0;
+	}
+}
 unsigned int counter = 0;
 
 // Main function
@@ -35,10 +75,4 @@ unsigned int counter = 0;
 int main(void) {
 	SystemCoreClockUpdate();
 	InitGPIO();
-	while(1) {
-		counter+=1;
-		if(counter > 0x0F) {
-			counter = 0;
-		}
-	}
 }
